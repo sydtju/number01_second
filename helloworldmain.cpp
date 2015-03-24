1b6888d23dee98513e27f0db9dc588d0d3f62620
sydtju <348829719@qq.com>
2015-03-24 20:50:57 +0800

    Update helloworldmain.cpp

--- helloworldmain.cpp
+++ helloworldmain.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(int argc,char**argv){
-  cout<<"hello world! this is second project!"<<endl;
+  cout<<"hello world! this is second project in github!"<<endl;
   return 0;
 }
